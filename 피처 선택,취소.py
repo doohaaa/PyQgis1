a5951fab1bee2c77ef8f389c597ca244a41fc952
doohaaa <102910408+doohaaa@users.noreply.github.com>
2022-10-18 18:52:12 +0900

    Delete 피처 선택,취소.py

--- 피처 선택,취소.py
+++
@@ -1,9 +0,0 @@
-#피처 선택(속성 선택)
-#Select features
-layer = iface.activeLayer()
-layer.selectByExpression('"lbl_doub">1500', QgsVectorLayer.SetSelection)
-
-
-##속성 선택 취소(피처 선택 취소)
-#Remove selection
-layer.removeSelection()
