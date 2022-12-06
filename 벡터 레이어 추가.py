b0c9eefaf2d5345f063a8b968f67566144812ee9
doohaaa <102910408+doohaaa@users.noreply.github.com>
2022-12-06 15:35:56 +0900

    Update 벡터 레이어 추가.py
    encoding 추가

--- 벡터 레이어 추가.py
+++ 벡터 레이어 추가.py
@@ -3,3 +3,7 @@
 
 fn="C:/Users/User/Desktop/지역분류체계/urban_emd_20/인구격자읍면동_20_부산/original"
 layer = iface.addVectorLayer(fn, '', 'ogr')
+
+## encoding
+layer.setProviderEncoding(u'UTF-8')
+layer.dataProvider().setEncoding(u'UTF-8')
